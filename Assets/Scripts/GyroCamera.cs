b58132f2e33072892d1e20663bb59b47d4b471f2
tylerkim <tylerkim129@gmail.com>
2023-07-18 21:49:33 +0900

    can change pet by wildpetID

--- Assets/Scripts/GyroCamera.cs
+++ Assets/Scripts/GyroCamera.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class GyroCamera : MonoBehaviour
 {
+    public string url = "http://172.10.5.110:80/map/show";
+    string petId = "64b13dce9a0458cf3b1e8cfd";
+    string locationId = "64b13a5c29beab0a894b8980";
     private Gyroscope gyro;
     private bool gyroSupported;
     private Quaternion rotFix;
@@ -13,7 +20,8 @@ public class GyroCamera : MonoBehaviour
     private float startY;
 
     [SerializeField]
-    private Transform zoomObj;
+    private GameObject[] zoomObj;
+    private GameObject currentZoomObj;
 
     // Start is called before the first frame update
     void Start() {
@@ -22,6 +30,7 @@ public class GyroCamera : MonoBehaviour
         GameObject camParent = new GameObject ("camParent");
         camParent.transform.position = transform.position;
         transform.parent = camParent.transform;
+        SetPetToShown();
 
         if (gyroSupported) {
             gyro = Input.gyro;
@@ -29,6 +38,53 @@ public class GyroCamera : MonoBehaviour
 
             camParent.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
             rotFix = new Quaternion (0, 0, 1, 0);
+        }
+    }
+
+    void SetPetToShown() {
+        WildPet newWildPet = new WildPet {
+            petId = petId,
+            locationId = locationId
+        };
+        string str = JsonUtility.ToJson(newWildPet);
+        var bytes = System.Text.Encoding.UTF8.GetBytes(str);
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "POST";
+        request.ContentType = "application/json";
+        request.ContentLength = bytes.Length;
+
+        using(var stream = request.GetRequestStream()) {
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+            stream.Close();
+        }
+
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+
+        var getdata = JsonUtility.FromJson<Response>(json);
+        Debug.Log(getdata);
+
+        zoomObj = new GameObject[3];
+        zoomObj[0] = GameObject.Find("Nupzuki");  
+        zoomObj[1] = GameObject.Find("Nupzuki2");
+        zoomObj[2] = GameObject.Find("Nupzuki3");
+        int curInt = 0;
+        if (getdata.pet.name == "새내기 넙죽이") {
+            curInt = 0;
+        } else if (getdata.pet.name == "화석 넙죽이") {
+            curInt = 1;
+        } else if(getdata.pet.name == "교수 넙죽이") {
+            curInt = 2;
+        }
+        // Let's assume we have three objects, we initialize them here
+        currentZoomObj = zoomObj[curInt];
+
+        for (int i = 0; i < 3; i++) {
+            if (i == curInt) continue;
+            else zoomObj[i].SetActive(false);
         }
     }
 
@@ -56,11 +112,19 @@ public class GyroCamera : MonoBehaviour
             hitPoint.y = 0;
 
             float z = Vector3.Distance(Vector3.zero, hitPoint);
-            zoomObj.localPosition = new Vector3(0f, zoomObj.localPosition.y, Mathf.Clamp(z, 8.5f, 8.5f));
+            // Here we choose which object to use. I used 0 as an example.
+            currentZoomObj.transform.localPosition = new Vector3(0f, currentZoomObj.transform.localPosition.y, Mathf.Clamp(z, 8.5f, 8.5f));
+            // zoomObj.localPosition = new Vector3(0f, zoomObj.localPosition.y, Mathf.Clamp(z, 8.5f, 8.5f));
         }
 
         startY = transform.eulerAngles.y;
         worldObj.rotation = Quaternion.Euler (0f, startY, 0f);
     }
+}
 
+[System.Serializable]
+public class getWildPetById
+{
+    public string petId;
+    public string locationId;
 }
