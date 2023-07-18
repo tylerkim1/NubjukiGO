d6f4bf929f5947f30b2ca363a8e3526e8e33d56f
seokyung1114 <seokyung1114@gmail.com>
2023-07-19 04:11:14 +0900

    fkfkkf

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActivateEvent(int eventID)
+    {
+        if (eventID == 1)
+        {
+            //SceneManager.LoadScene("Goldsmiths Student Union");
+        }
+        else if (eventID == 2)
+        {
+            //SceneManager.LoadScene("Goldsmiths Student Union");
+        }
+    }
+}
